5729aee0eba94ce816c61091e1bbae0b1658a042
AntonZykov85 <zakupki_pomosh@mail.ru>
2022-02-10 15:30:47 +0300

    lesson_3_done

---
+++ to_do/project/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.viewsets import ModelViewSet
+from .models import Project, ToDo
+from .serializers import ProjectModelSerializer, ToDoModelSerializer
+
+
+class ProjectModelViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+
+class ToDoModelViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoModelSerializer
