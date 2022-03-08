6ef5acf18bf2ea7c62f5decde9a50693c1a4d5c1
AntonZykov85 <zakupki_pomosh@mail.ru>
2022-03-08 21:42:00 +0300

    lesson_7_done

--- to_do/project/tests.py
+++ to_do/project/tests.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory,force_authenticate,APIClient,APISimpleTestCase,APITestCase
+from mixer.backend.django import mixer
+from users.views import UserModelViewSet
+from users.models import User
+from project.views import ProjectModelViewSet, ToDoModelViewSet
+from project.models import Project, ToDo
+from rest_framework import status
+from rest_framework.test import APIRequestFactory,force_authenticate,APIClient,APISimpleTestCase,APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+
+class TestProjects(APITestCase):
+
+    def setUp(self) -> None:
+        self.name = 'admin'
+        self.password = 'password'
+        self.email = 'admin@gmail.com'
+
+        self.data = {'username': 'user', 'first_name': '', 'last_name': '', 'email': 'randommail@gmail.com'}
+        self.data_put = {'username': 'user_1', 'first_name': '', 'last_name': '', 'email': 'randommail_1@gmail.com'}
+        self.url = '/api/users/'
+        self.admin = User.objects.create_superuser(self.name, self.email, self.password)
+
+    def test_get_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+    def test_put_admin(self):
+        user = User.objects.create(**self.data)
+        project = Project.objects.create(text='test', user=user)
+        self.client.login(username=self.name,password=self.password)
+        response = self.client.put(f'{self.url}{project.id}/', {'text': 'Project_text', 'user': project.user.id})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+        project_ =Project.objects.get(id=project.id)
+        self.assertEqual(project_.text, 'Project_text')
+        self.client.logout()
+
+    def test_put_mixer(self):
+
+        project = mixer.blend(Biography)
+        self.client.login(username=self.name, password=self.password)
+        response = self.client.put(f'{self.url}{project.id}/', {'text': 'Project_text', 'user': project.user.id})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        project_ = Project.objects.get(id=project.id)
+        self.assertEqual(project_.text, 'Project_text')
+        self.client.logout()
+
+    def test_put_mixer_fields(self):
+        project = mixer.blend(Project,text='Project_text')
+        self.assertEqual(project.text, 'Project_text')
+        self.client.login(username=self.name, password=self.password)
+        response = self.client.put(f'{self.url}{project.id}/', {'text': 'Project_text', 'user': project.user.id})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        project_ = Project.objects.get(id=project.id)
+        self.assertEqual(project_.text, 'Project_text')
+        self.client.logout()
--- to_do/users/tests.py
+++ to_do/users/tests.py
@@ -1,3 +1,84 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory,force_authenticate,APIClient,APISimpleTestCase,APITestCase
+from mixer.backend.django import mixer
+
+import users
+from users.views import UserModelViewSet
+from users.models import User
+from project.views import ProjectModelViewSet, ToDoModelViewSet
+from project.models import Project,ToDo
+from rest_framework import status
+from rest_framework.test import APIRequestFactory,force_authenticate,APIClient,APISimpleTestCase,APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+class TestAuthorViewSet(TestCase):
+
+    def setUp(self) -> None:
+        self.name = 'admin'
+        self.password = 'password'
+        self.email = 'admin@gmail.com'
+
+        self.data = {'username': 'user', 'first_name': '', 'last_name': '', 'email': 'randommail@gmail.com'}
+        self.data_put = {'username': 'user_1', 'first_name': '', 'last_name': '', 'email': 'randommail_1@gmail.com'}
+        self.url = '/api/users/'
+        self.admin = User.objects.create_superuser(self.name, self.email, self.password)
+
+    #APIRequestFactory force_authenticate
+    def test_get_list(self):
+        #
+        factory = APIRequestFactory()
+        request = factory.get(self.url)
+        view = UserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_guest(self):
+        factory = APIRequestFactory()
+        request = factory.post(self.url, self.data, format='json')
+        view = UserModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_admin(self):
+        factory = APIRequestFactory()
+        request = factory.post(self.url, self.data, format='json')
+        force_authenticate(request, self.admin)
+        view = UserModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+#APIClient
+    def test_get_detail(self):
+
+        client = APIClient()
+        user = User.objects.create(**self.data)
+        response = client.get(f'{self.url}{user.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_put_guest(self):
+        client = APIClient()
+        user = User.objects.create(**self.data)
+        response = client.put(f'{self.url}{user.id}/',self.data_put)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_put_admin(self):
+        client = APIClient()
+        user = User.objects.create(**self.data)
+        client.login(username=self.name, password=self.password)
+        response = client.put(f'{self.url}{user.id}/', self.data_put)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        user_ = User.objects.get(id=user.id)
+        self.assertEqual(user_.username, self.data_put.get('username'))
+        self.assertEqual(user_.first_name, self.data_put.get('first_name'))
+        self.assertEqual(user_.last_name, self.data_put.get('last_name'))
+        self.assertEqual(user_.email, self.data_put.get('email'))
+
+        client.logout()
+
+    def tearDown(self) -> None:
+        pass
